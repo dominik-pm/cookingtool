f5221d3750abda03cb0f257558f8d9601b291a1a
Dominik Pömmer <46850095+dominik-pm@users.noreply.github.com>
2019-03-29 18:52:43 +0100

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -2,38 +2,84 @@ let socket;    // define a socket variable
 
 function setup() {
   // connect to server via sockets
-  socket = io.connect('http://www.cookingtool.tk'); 
-//   socket = io.connect('localhost:80'); // local
+//   socket = io.connect('http://www.cookingtool.tk'); 
+  socket = io.connect('localhost:80'); // local
 
   // trigger 'updateMeals' when this client recieves a message called 'updateMeals'
   socket.on('updateMeals', updateMeals); // same message name as in server
+  socket.on('setMealToday', setMealToday);
+  socket.on('alertAddMeal', alertAddMeal);
+  socket.on('alertGeneral', alertGeneral);
+  socket.on('unsetMealToday', unsetMealToday);
 
-  console.log('connected to server');
+  console.log('connected to server! ');
+
+  noCanvas();
 }
 
 let meals;
 
-function updateMeals(meals) {
-  meals = meals;
-  displayMeals('.wrapperMealList', meals);
-  console.log('updated all meals!');
+// --- server called functions ---
+function updateMeals([meals, mealSuggestedIndex]) {
+    meals = meals;
+    displayMeals('.wrapperMealList', meals);
+    displayMeals('.wrapperMealSuggested', [meals[mealSuggestedIndex]]);
+    updateMealTodayPlaceholder(meals);
+    console.log('updated all meals!');
 }
+function setMealToday(mealToday) {
+    console.log('set meal to cook for today')
+    $('.mealTodayPlaceholder').hide();              // hide default stuff when there is no meal for today
+    displayMeals('.wrapperMealToday', [mealToday]); // add the meal to the container
+    $('.wrapperMealToday').show();                  // show the meal today wrapper
+    $('#btn-unsetMealToday').show();                // show the button to unselect today's meal
+    $('#btn-unsetMealToday').click(function() {     // eventlistener to that button
+        socket.emit('req-unsetMealToday');
+    });
+}
+function unsetMealToday() {
+    console.log('unset meal to cook for today')
+    $('.wrapperMealToday').hide();
+    $('#btn-unsetMealToday').show();
+    $('.mealTodayPlaceholder').show();
+}
+function alertAddMeal([dangerBool, msg]) {
+    let alertBox;
+    if (dangerBool) {
+        alertBox = $('.alert-danger-modal');
+    }
+    else {
+        alertBox = $('.alert-success-modal');
+    }
+    alertBox.text(msg);
+    alertBox.slideDown("slow");
+    setTimeout(function() {
+        alertBox.fadeOut(1000);
+    }, 2500);
+}
+function alertGeneral([dangerBool, msg]) {
+    let alertBox;
+    if (dangerBool) {
+        alertBox = $('.alert-danger-general');
+    }
+    else {
+        alertBox = $('.alert-success-general');
+    }
+    alertBox.find('p').text(msg);
+    alertBox.slideDown("slow");
+    setTimeout(function() {
+        alertBox.fadeOut(1000);
+    }, 2500);
+}
+// --- ---
 
-// function draw() {
-
-// }
 
 window.onload = function() {
-    $('#meal-date').val(dateToday());
-    // displayMeals('.wrapperMealList', meals);
-    // displayMeals('.wrapperBestMeal', bestMeal);
+    $('#meal-date').val(getDateToday());
+    $('.alert').hide();
+    $('#btn-unsetMealToday').hide();
+    setPassword();
 };
-
-function dateToday() {
-    let today = new Date().toJSON().slice(0,10).replace(/-/g,'-');
-    // console.log(today);
-    return today;
-}
 
 $('#btn-saveChanges').click(function() {
     let inputMealName = $('#meal-name').val();
@@ -41,9 +87,9 @@ $('#btn-saveChanges').click(function() {
     let inputMealDescription = $('#meal-description').val();
     $('#meal-description').val('');
     let inputMealDate = $('#meal-date').val();
-    $('#meal-date').val(dateToday());
+    $('#meal-date').val(getDateToday());
     
-    console.log('Requesting to add a new Meal: Schnitzl');
+    console.log('Requesting to add a new Meal: \'' + inputMealName +'\'');
 
     let newMeal = {
       name: inputMealName,
@@ -55,9 +101,47 @@ $('#btn-saveChanges').click(function() {
 
     console.log(newMeal);
     
-    socket.emit('addMeal', newMeal);   // send "newMeal", messagename: "addMeal"
+    socket.emit('req-addMeal', newMeal);   // send "newMeal", messagename: "addMeal"
 });
 
+$('#btn-cookSuggested').click(function() {
+    socket.emit('req-setMealTodaySuggested');
+    // console.log('requested to cook suggested meal today');
+});
+
+function updateMealTodayPlaceholder(meals) {
+    let dropdownMenu = $('.dropdown-menu');
+
+    dropdownMenu.children().remove();
+
+    for (let i = 0; i < meals.length; i++) {
+        dropdownMenu.append('<a class="dropdown-item" href="#">'+ meals[i].name +'</a>');
+    }
+
+    $('.dropdown-item').click(function() {
+        let index = 0;
+
+        let dropdownItems = $($(this).parent()).children();
+
+        for (let i = 0; i < dropdownItems.length; i++) {
+            if (dropdownItems[i].innerText == this.innerText) {
+                index = i;
+            }
+        }
+
+        socket.emit('req-setMealToday', index);
+    });
+}
+function setPassword() {
+    let alertBox = $('.alert-info');
+    alertBox.slideDown("slow");
+    $('#btnPasswordSubmit').click(function() {
+        let pwInput = $('#inputPassword');
+        alertBox.fadeOut(1000);
+        socket.emit('req-setPassword', pwInput.val());
+        pwInput.val('');
+    });
+}
 function displayMeals(container, meals) {
     $(container).children().remove();
 
@@ -65,37 +149,37 @@ function displayMeals(container, meals) {
     for (let i = 0; i < meals.length; i++) {
         ratingStrings[i] = meals[i].ratings;
     }
-
+    
     for (let i = 0; i < meals.length; i++) {
         $(container).append(
             '<div class="mealContainer rounded-lg bg-dark text-white">' +
                 '<div class="mealHeader">' +
-                    '<img src="assets/schnitzel.jpg" class="mr-2 card-img-left rounded-lg" alt="schnitzel">' +
-                    '<h2>'+meals[i].name+'</h2>'+
+                '<img src="assets/mealIcon.png" class="mr-2 card-img-left rounded-lg" alt="schnitzel">' +
+                '<h2>'+meals[i].name+'</h2>'+
                 '</div>'+
                 '<div class="mealMain d-flex">'+
+                '<div>'+
+                '<h4>Last Cook</h4>'+
+                '<p>'+meals[i].date+'</p>'+
+                '</div>'+
                     '<div>'+
-                        '<h4>Last Cook</h4>'+
-                        '<p>'+meals[i].date+'</p>'+
-                    '</div>'+
-                    '<div>'+
-                        '<h4>Rate</h4>'+
-                        '<button class="btn btn-success btn-rate1" value='+i+'>1</button>'+
-                        '<button class="btn btn-warning btn-rate2" value='+i+'>2</button>'+
-                        '<button class="btn btn-info btn-rate3" value='+i+'>3</button>'+
-                        '<button class="btn btn-primary btn-rate4" value='+i+'>4</button>'+
+                    '<h4>Rate</h4>'+
+                    '<button class="btn btn-success btn-rate1" value='+i+'>1</button>'+
+                    '<button class="btn btn-warning btn-rate2" value='+i+'>2</button>'+
+                    '<button class="btn btn-info btn-rate3" value='+i+'>3</button>'+
+                    '<button class="btn btn-primary btn-rate4" value='+i+'>4</button>'+
                         '<button class="btn btn-danger btn-rate5" value='+i+'>5</button>'+
-                    '</div>'+
-                    '<div>'+
+                        '</div>'+
+                        '<div>'+
                         '<h4>Rating</h4>'+
                         '<p>'+ meals[i].rating +'</p>'+
-                    '</div>'+
+                        '</div>'+
                 '</div>'+
                 '<div class="mealFooter">'+
-                    '<div>'+
+                '<div>'+
                         '<h5>Description</h5>'+
                         '<ul>'+
-                            '<li>'+meals[i].description+'</li>'+
+                        '<li>'+meals[i].description+'</li>'+
                         '</ul>'+
                     '</div>'+
                     '<p>Ratings: '+ ratingStrings[i] +'</p>'+
@@ -103,53 +187,56 @@ function displayMeals(container, meals) {
             '</div>'
         );
     }
-    
+
     // Rating
     // TODO: maybe more dynamic
     // get somehow the rating (1-5)
     // 'mealIndex' = +$(this).val();    // + converts the value of the button to a number
     // 'rateMeal': [index of the meal, rating]
-
     $('.btn-rate1').click(function() {
         let mealIndex = +$(this).val();
-        socket.emit('rateMeal', [mealIndex, 1]);
+        socket.emit('req-rateMeal', [mealIndex, 1]);
     });
     $('.btn-rate2').click(function() {
         let mealIndex = +$(this).val();
-        socket.emit('rateMeal', [mealIndex, 2]);
+        socket.emit('req-rateMeal', [mealIndex, 2]);
     });
     $('.btn-rate3').click(function() {
         let mealIndex = +$(this).val();
-        socket.emit('rateMeal', [mealIndex, 3]);
+        socket.emit('req-rateMeal', [mealIndex, 3]);
     });
     $('.btn-rate4').click(function() {
         let mealIndex = +$(this).val();
-        socket.emit('rateMeal', [mealIndex, 4]);
+        socket.emit('req-rateMeal', [mealIndex, 4]);
     });
     $('.btn-rate5').click(function() {
         let mealIndex = +$(this).val();
-        socket.emit('rateMeal', [mealIndex, 5]);
+        socket.emit('req-rateMeal', [mealIndex, 5]);
     });
-
-
-    // footer display
-    let toggleFooter = false;
     
-    let footers = $('.mealFooter');
+    
+    // hide all meal footers by default
+    let footers = $(container + ' .mealFooter');
     for (let i = 0; i < footers.length; i++) {
         $(footers[i]).hide();
     }
-    
-    $('.mealHeader').click(function() {
-        let footer = $(this).parent().find('.mealFooter');
-        if (toggleFooter) {
-            footer.fadeOut();
-            toggleFooter = false;
+
+    // slide down/up meal footer when clicked on meal header
+    $(container + ' .mealHeader').click(function() {
+        let collapse = $(this).parent().find('.mealFooter');
+        // collapse: element to collapse
+        if ($(collapse).is(':visible')) {
+            collapse.slideUp();
+            footerVisible = false;
         }
         else {
-            footer.fadeIn();
-            toggleFooter = true;
+            collapse.slideDown();
+            footerVisible = true;
         }
     });
-    //
+}
+function getDateToday() {
+    let today = new Date().toJSON().slice(0,10).replace(/-/g,'-');
+    // console.log(today);
+    return today;
 }
--- server.js
+++ server.js
@@ -14,75 +14,231 @@ var io = socket(server);
 
 io.sockets.on('connection', newConnection);
 
-
 // --- reading from the meals.json file ---
   let fs = require('fs');                   // import the FileSystem of NodeJS
   let data = fs.readFileSync('meals.json'); // read the current meals (raw json data)
   let meals = JSON.parse(data);             // parse the raw data so meals is a JS Array with the meal objects in it
-  
+
   // meals.sort((a, b) => a.rating - b.rating);// sort by rating (best to worst)
   
-  updateMeals();
-
   // readFileSync vs readFile:
   // with sync the next line of code gets executed when the file has been fully read
   // with asynchronos sync you can still listen to other stuff while loading the json
-/// --- ---
+// --- ---
+
+let mealTodayIndex;
+let mealTodaySet = false;
+let mealSuggestedIndex;
+globalUpdateMeals();
 
 function newConnection(socket) {
   console.log('new connection: ' + socket.id);
 
-  socket.emit('updateMeals', meals);  // give the client the meals once its connected
-  // (only this client (currently to all))
+  // check user validity
+  let validUser = false;
+  let validAdmin = false;
+  //
+  
+  // trigger these functions if there are that messages frin the client
+  socket.on('req-addMeal', addMeal);
+  socket.on('req-rateMeal', rateMeal);
+  socket.on('req-setPassword', setPassword);
+  socket.on('req-setMealToday', setMealToday);
+  socket.on('req-unsetMealToday', unsetMealToday);
+  socket.on('req-setMealTodaySuggested', setMealTodaySuggested);
+  //
 
-  // if there is a message called 'addMeal' from this client --> trigger the 'addMeal'-function
-  socket.on('addMeal', addMeal);
+  // give the client the meals once he's connected
+  socket.emit('updateMeals', [meals, mealSuggestedIndex]); 
 
-  socket.on('rateMeal', rateMeal);
+  emitMealToday();
 
+  var date = new Date();
+  var current_hour = date.getHours();
+  if (current_hour >= 16) { 
+    console.log('unsetting meal today, time: ' + current_hour);
+    io.sockets.emit('unsetMealToday');
+  }
 
+  // client called functions
   function addMeal(newMeal) {
-    console.log('received a new meal: ' + newMeal.name);
-    // maybe some checking
-    // if (newMeal.name) ...
-    // if all good: write to json and send a message 'updateMeals' to all CLients
-    // {
-      // --- writing to the meals.json file ---
-        meals.push(newMeal);                          // add the new meal to the array from the json file
-        let data = JSON.stringify(meals, null, 2);    // stringify the new meals array
-        fs.writeFile('meals.json', data, finished);   // save the stringified JSON
-        function finished(error) {
-          console.log(newMeal.name + ' added to database!');
+    // check validity of new meal
+    if (validAdmin) {
+
+      let mealNameValid = true;
+
+      // name should have at least 3 letters
+      if (newMeal.name.length < 3) {
+        mealNameValid = false;
+      }
+
+      // name should not be a duplicant
+      for (let i = 0; i < meals.length; i++) {
+        if (meals[i].name == newMeal.name) {
+          mealNameValid = false;
         }
-        // --- ---
+      }
+
+      if (mealNameValid) {
+        // if 'newMeal' is valid
+        //  -> write to json and send update all meals
         
-        updateMeals();
-    // }
-    // if anything wrong: send error message to this client
-    // {
-      //socket.emit('newMealError', errorMsg); //--> goes to everyone including this client
-    //}
+        // --- writing to the meals.json file ---
+          meals.push(newMeal);                          // add the new meal to the array from the json file
+          let data = JSON.stringify(meals, null, 2);    // stringify the new meals array
+          fs.writeFile('meals.json', data, finished);   // save the stringified JSON
+          function finished(error) {
+            console.log(newMeal.name + ' added to database!');
+          }
+        // --- ---
+
+        globalUpdateMeals();
+
+        let msg = 'Added ' + newMeal.name + ' to database!';
+        socket.emit('alertAddMeal', [false, msg])  // [dangerBool/*'true if its an error'*/, msg]
+      }
+      else {
+        // if invalid meal name:
+        //  -> send error message to this client
+        let msg = 'Couldn\'t add this meal to database. Invalid meal name!';
+        socket.emit('alertAddMeal', [true, msg])  // [dangerBool/*'true if its an error'*/, msg]
+      }
+    }
+    else {
+      // if no admin permissions:
+      //  -> send error message to this client
+      let msg = 'Couldn\'t add this meal to database. You are not a valid admin!';
+      socket.emit('alertAddMeal', [true, msg])  // [dangerBool/*'true if its an error'*/, msg]
+    }
   }
+  function rateMeal([index, rating]) {
+    let msg = '';
+  
+    // check for rating validity 
+    if (rating != null && rating >= 1 && rating <= 5) {
+      // check for index validity 
+      if (index < meals.length && index >= 0) {
+        // check user validity 
+        if (validUser) {
+          // add the new rating to the meal with the given index
+          meals[index].ratings.unshift(rating);
+          // max ratings: 10
+          if (meals[index].ratings.length > 10) {
+            meals[index].ratings.pop();
+          }
+
+          globalUpdateMeals();
+  
+          msg += 'Rated \'' + meals[index].name + '\' a ' + rating + '!';
+          socket.emit('alertGeneral', [false, msg])
+        }
+        else {
+          msg += 'You are not a valid user!';
+          socket.emit('alertGeneral', [true, msg])
+        }
+      }
+      else {
+        msg += 'Index not valid!';
+        socket.emit('alertGeneral', [true, msg])
+      }
+    }
+    else {
+      msg += 'Rating not valid!';
+      socket.emit('alertGeneral', [true, msg])
+    }
+  }
+  function setPassword(pw) {
+    let validUserPW = '321';
+    let validAdminPW = '987654321';
+    if (pw == validUserPW) {
+      validUser = true;
+      console.log('user logon');
+    }
+    else if (pw == validAdminPW) {
+      validUser = true;
+      validAdmin = true;
+      console.log('admin logon');
+    }
+    else {
+      console.log('user wrong pw');
+    }
+  }  
+  function setMealToday(index) {
+    if (validAdmin) {
+      if (!mealTodaySet) {
+        if (index < meals.length) {
+          mealTodayIndex = index;
+          mealTodaySet = true;
+          emitMealToday();
+          console.log('set meal today:');
+          console.log(meals[mealTodayIndex]);
+          let msg = 'Successfully set today\'s meal!'
+          socket.emit('alertGeneral', [false, msg]);
+        }
+        else {
+          let msg = 'Index too high (you are as well probably :) )!';
+          socket.emit('alertGeneral', [true, msg]);
+        }
+      }
+      else {
+        let msg = 'Meal already set to be cooked today!';
+        socket.emit('alertGeneral', [true, msg]);
+      }
+    }
+    else {
+      let msg = 'You are not a valid admin!';
+      socket.emit('alertGeneral', [true, msg]);
+    }
+  }
+  function unsetMealToday() {
+    if (validAdmin) {
+      if (mealTodaySet) {
+        mealTodaySet = false;
+        io.sockets.emit('unsetMealToday');
+        console.log('unset meal today');
+        let msg = 'Successfully unset today\'s meal!'
+        socket.emit('alertGeneral', [false, msg]);
+      }
+    }
+    else {
+      let msg = 'You are not a valid admin!';
+      socket.emit('alertGeneral', [true, msg]);
+    }
+  }
+  function setMealTodaySuggested() {
+    setMealToday(mealSuggestedIndex);
+    console.log('set meal suggested to be cooked today');
+  }
+  //
 }
 
-function rateMeal([index, rating]) {
-  // add the new rating to the meal with the given index
-  meals[index].ratings.unshift(rating);
-  // max ratings: 10
-  if (meals[index].ratings.length > 10) {
-    meals[index].ratings.pop();
-  }
-  updateMeals();
-} 
-
-function updateMeals() {
+function globalUpdateMeals() {
   sortMealsByDate();
-  calculateRating();
-  // socket.broadcast.emit('updateMeals', meals);    // send data to all other clients, could be renamed or changed
-  io.sockets.emit('updateMeals', meals); //--> goes to everyone including this client
+  updateRating();
+  updateSuggested();
+  emitMealToday();
+  io.sockets.emit('updateMeals', [meals, mealSuggestedIndex]);
 }
 
-function calculateRating() {
+function emitMealToday() {
+  if (mealTodaySet) {
+    io.sockets.emit('setMealToday', meals[mealTodayIndex]);
+  }
+}
+
+function updateSuggested() {
+  let startIndex = Math.floor(meals.length/2);
+
+  mealSuggestedIndex = meals.length-1;
+
+  for (let i = meals.length-1; i >= startIndex; i--) {
+    if (meals[i].rating < meals[mealSuggestedIndex].rating || meals[mealSuggestedIndex].rating == 0) {
+      mealSuggestedIndex = i;
+    }
+  }
+}
+
+function updateRating() {
   for (let i = 0; i < meals.length; i++) {
     if (meals[i].ratings.length < 1) {
       meals[i].rating = 0;
@@ -111,5 +267,4 @@ function sortMealsByDate() {
         return 0;
     }
   });
-  console.log(sortedMeals);
 }
