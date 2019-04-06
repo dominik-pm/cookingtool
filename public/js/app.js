085aea0f377488cd54c674aa6bc150ee60dbe9d9
Dominik Pömmer <46850095+dominik-pm@users.noreply.github.com>
2019-04-06 09:24:05 +0200

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,9 @@ let socket;    // define a socket variable
 function setup() {
     // connect to server via sockets
     // socket = io.connect('http://37.120.137.243:13181');
-    socket = io.connect('http://cookingtool.tk');
+    // socket = io.connect('http://cookingtool.tk');
     //socket = io.connect('192.168.1.133:80');
-    // socket = io.connect('localhost:80'); // local
+    socket = io.connect('localhost:80'); // local
 
     // trigger 'updateMeals' when this client recieves a message called 'updateMeals'
     socket.on('updateMeals', updateMeals); // same message name as in server
